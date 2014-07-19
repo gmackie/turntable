da58a41ab59f6b69763c3c9f937e7411ab363a34
Graham Mackie <mackieg@umich.edu>
2014-07-19 16:19:52 -0400

    workable prototype

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import psycopg2
-
 import time
-import db
+from hashlib import md5
 from flask import Flask
 from flask.ext.restful import reqparse, abort, Api, Resource
 from youtube_dl import YoutubeDL
 from youtube_dl import FFmpegExtractAudioPP
+import redis
 import boto
 import boto.s3.connection
 from boto.s3.key import Key
@@ -24,12 +23,16 @@ api = Api(app)
 conn = boto.connect_s3()
 bucket = conn.get_bucket('turntable.dongs.in')
 
+r = redis.Redis("localhost")
 
 def abort_if_song_doesnt_exist(song_id):
         abort(404, message="song {} doesn't exist".format(song_id))
 
 parser = reqparse.RequestParser()
 parser.add_argument('hash', type=str)
+parser.add_argument('username', type=str)
+parser.add_argument('password', type=str)
+parser.add_argument('room', type=str)
 
 
 # Todo
@@ -37,45 +40,46 @@ parser.add_argument('hash', type=str)
 class Song(Resource):
     def get(self, song_id):
         args = parser.parse_args()
-        song = db.session.query(db.Song).filter(db.Song.yt_hash == song_id).first()
-        url = key.generate_url(0, query_auth=False, force_http=True)
-        if song is None:
+        if r.sismember("songs", song_id):
+            title = r.hget("song:%s" % song_id, "title")
+            song_title = r.hget("song:%s" % song_id, "song_title")
+            artist = r.hget("song:%s" % song_id, "artist")
+            yt_hash = r.hget("song:%s" % song_id, "yt_hash")
+
+            key = bucket.get_key(song.yt_hash + '.mp3')
+            url = key.generate_url(0, query_auth=False, force_http=True)
+            ret_song = {
+                'title': title,
+                'song_title': song_title,
+                'artist': artist,
+                'yt_hash': yt_hash,
+                'url': url,
+            }
+        else:
             abort(404, message="error song {} not in db!".format(song_id))
-        ret_song = {
-            'title': song.title,
-            'song_title': song.song_title,
-            'artist': song.artist,
-            'yt_hash': song.yt_hash,
-            'url': url,
-        }
-        
         return ret_song, 200
-
-
-    def put(self, song_id):
-        args = parser.parse_args()
-        song = {
-            'title': args['title'],
-            'artist': args['artist'],
-            'length': args['length'],
-        }
-        return song, 201
 
 
 # TodoList
 #   shows a list of all todos, and lets you POST to add new tasks
 class SongList(Resource):
     def get(self):
-        songs = db.session.query(db.Song)
         ret_songs = []
-        for song in songs:
-            key = bucket.get_key(song.yt_hash + '.mp3')
+        song_hashes = r.smembers("songs")
+        for song_id in song_hashes:
+            title = r.hget("song:%s" % song_id, "title")
+            song_title = r.hget("song:%s" % song_id, "song_title")
+            artist = r.hget("song:%s" % song_id, "artist")
+            yt_hash = r.hget("song:%s" % song_id, "yt_hash")
+            
+            key = bucket.get_key(song_id + '.mp3')
             url = key.generate_url(0, query_auth=False, force_http=True)
+            
             ret_song = {
-                'title': song.title,
-                'song_title': song.song_title,
-                'artist': song.artist,
-                'yt_hash': song.yt_hash,
+                'title': title,
+                'song_title': song_title,
+                'artist': artist,
+                'yt_hash': yt_hash,
                 'url': url,
             }
             ret_songs.append(ret_song)
@@ -84,39 +88,304 @@ class SongList(Resource):
     def post(self):
         args = parser.parse_args()
         song_id = args['hash']
-        song = db.session.query(db.Song).filter(db.Song.yt_hash == args['hash']).first()
-        if song is None:
+        if r.sadd("songs", song_id):
             info = ydl.extract_info('http://www.youtube.com/watch?v=' + args['hash'],  download=True)
-            song = db.Song( 
-                title=info['title'],
-                song_title='',
-                artist='',
-                yt_hash=args['hash'],
-            )
-            db.session.add(song)
-            db.session.commit()
+            
+            r.hset("song:%s" % song_id, "title", info['title'])
+            r.hset("song:%s" % song_id, "song_title", '')
+            r.hset("song:%s" % song_id, "artist", '')
+            r.hset("song:%s" % song_id, "yt_hash", args['hash'])
+            
             k = Key(bucket)
             k.key = args['hash'] + '.mp3'
             k.set_contents_from_filename(args['hash'] + '.mp3')
             url = k.generate_url(0, query_auth=False, force_http=True)
+            
+            r.set("song:%s:url" % song_id, url)
+            
             ret_song = {
-            'title': song.title,
-            'song_title': song.song_title,
-            'artist': song.artist,
-            'yt_hash': song.yt_hash,
+            'title': info['title'],
+            'song_title': '',
+            'artist': '',
+            'yt_hash': song_id,
             'url': url,
-        }
+            'success': True,
+            }
+        else:
+            title = r.hget("song:%s" % song_id, "title")
+            song_title = r.hget("song:%s" % song_id, "song_title")
+            artist = r.hget("song:%s" % song_id, "artist")
+            yt_hash = r.hget("song:%s" % song_id, "yt_hash")
+            url = r.hget("song:%s" % song_id, "url")
+            
+            ret_song = {
+                'title': title,
+                'song_title': song_title,
+                'artist': artist,
+                'yt_hash': yt_hash,
+                'url': url,
+                'success': False,
+                'uploaded': True,
+            }
         
         return ret_song, 201
 
+class User(Resource):
+    def get(self, username):
+        args = parser.parse_args()
+        if r.sismember("users", username):
+            plays = r.hget("user:%s" % username, "plays")
+            points = r.hget("user:%s" % username, "points")
+            skips = r.hget("user:%s" % username, "skips")
+            created_on = r.hget("user:%s" % username, "created_on")
+            
+            ret_user = {
+                'username': username,
+                'created_on': created_on,
+                'plays': plays,
+                'points': points,
+                'skips': skips,
+            }
+        else:
+            abort(404, message="error song {} not in db!".format(song_id))
+        return ret_user, 200
+
+
+class UserList(Resource):
+    def get(self):
+        ret_users = []
+        users = r.smembers("users")
+        for username in users:
+            plays = r.hget("user:%s" % username, "plays")
+            points = r.hget("user:%s" % username, "points")
+            skips = r.hget("user:%s" % username, "skips")
+            created_on = r.hget("user:%s" % username, "created_on")
+            
+            ret_user = {
+                'username': username,
+                'created_on': created_on,
+                'plays': plays,
+                'points': points,
+                'skips': skips,
+            }
+            ret_users.append(ret_users)
+        return ret_users
+
+    def post(self):
+        args = parser.parse_args()
+        username = args['username']
+        password = args['password']
+        print username
+        print password
+        if r.sadd("users", username):
+            ts = time.time()
+            timestamp = int(ts)
+            r.hset("user:%s" % username, "created_on", timestamp)
+            r.hset("user:%s" % username, "plays", 0)
+            r.hset("user:%s" % username, "points", 0)
+            r.hset("user:%s" % username, "skips", 0)
+            r.hset("user:%s" % username, "password", md5(password).hexdigest())
+                
+            ret_user = {
+            'username': username,
+            'created_on': timestamp,
+            'plays': 0,
+            'points': 0,
+            'skips': 0,
+            }
+        else:
+            abort(404, message="{'error': 'user {} already in db'}".format(username))
+        
+        return ret_user, 201
+
+class Queue(Resource):
+    def get(self, username):
+        ret_songs = []
+        song_hashes = r.lrange("queue:%s" % username, 0 ,-1)
+        for song_id in song_hashes:
+            title = r.hget("song:%s" % song_id, "title")
+            song_title = r.hget("song:%s" % song_id, "song_title")
+            artist = r.hget("song:%s" % song_id, "artist")
+            yt_hash = r.hget("song:%s" % song_id, "yt_hash")
+            
+            key = bucket.get_key(song_id + '.mp3')
+            url = key.generate_url(0, query_auth=False, force_http=True)
+            
+            ret_song = {
+                'title': title,
+                'song_title': song_title,
+                'artist': artist,
+                'yt_hash': yt_hash,
+                'url': url,
+            }
+            ret_songs.append(ret_song)
+        return ret_songs
+
+    def post(self, username):
+        args = parser.parse_args()
+        song_id = args['hash']
+        username = username
+
+        if r.lpush("queue:%s" % username, song_id):
+            ret = {
+            'song': song_id,
+            'added': 1,
+            }
+        else:
+            abort(404, message="{'error': 'user {} already in db'}".format(username))
+        
+        return ret, 201
+
+class Room(Resource):
+    def get(self, room):
+        args = parser.parse_args()
+        if r.sismember("rooms", room):
+            users = r.hget("room:%s" % room, "users")
+            current_song = r.hget("room:%s:" % room, "current_song")
+            favorite_song = r.hget("room:%s" % room, "favorite_song")
+            created_on = r.hget("room:%s" % room, "created_on")
+            
+            ret_room = {
+                'room': room,
+                'users': users,
+                'created_on': created_on,
+                'current_song': current_song,
+                'favorite_song': favorite_song,
+            }
+        else:
+            abort(404, message="error song {} not in db!".format(song_id))
+        return ret_room, 200
+
+
+class RoomList(Resource):
+    def get(self):
+        ret_rooms = []
+        rooms = r.smembers("rooms")
+        for room in rooms:
+            users = r.hget("room:%s" % room, "users")
+            current_song = r.hget("room:%s:" % room, "current_song")
+            favorite_song = r.hget("room:%s" % room, "favorite_song")
+            created_on = r.hget("room:%s" % room, "created_on")
+            
+            ret_room = {
+                'room': room,
+                'users': users,
+                'created_on': created_on,
+                'current_song': current_song,
+                'favorite_song': favorite_song,
+            }
+            ret_rooms.append(ret_room)
+        return ret_rooms
+
+    def post(self):
+        args = parser.parse_args()
+        room = args['room']
+        if r.sadd("rooms", room):
+            ts = time.time()
+            timestamp = int(ts)
+            r.hset("room:%s" % room, "created_on", timestamp)
+            r.hset("room:%s" % room, "users", 0)
+            r.hset("room:%s" % room, "current_song", '')
+            r.hset("room:%s" % room, "favorite_song", '')
+                
+            ret_room = {
+                'room': room,
+                'users': 0,
+                'created_on': timestamp,
+                'current_song': '',
+                'favorite_song': '',
+            }
+        else:
+            abort(404, message="{'error': 'user {} already in db'}".format(room))
+        
+        return ret_room, 201
+
+class Skip(Resource):
+    def post(self, room):
+        args = parser.parse_args()
+        username = args['username']
+        song_id = r.hget("room:%s" % room, "current_song")
+        
+        ret= {
+            'song': song_id,
+            'numSkips': skips,
+            'reqSkips': reqSkips,
+        }
+        return ret, 200
+
+class Join(Resource):
+    def post(self, room):
+        args = parser.parse_args()
+        username = args['username']
+        if r.sismember("rooms", room):
+            if r.sismember("users", username):
+                r.sadd("djset:%s" % room, username) 
+                numDj = r.lpush("djlist:%s" % room, username)
+                ret= {
+                    'room': room,
+                    'success': True,
+                    'numDj': numDj,
+                    'user': username,
+                }
+
+            else:
+                ret= {
+                    'room': room,
+                    'success': False,
+                    'error': 'user does not exist',
+                    'user': username,
+                }
+        else:
+            ret= {
+                'room': room,
+                'success': False,
+                'error': 'room does not exist',
+                'user': username,
+            }
+        return ret, 200
+
+class Leave(Resource):
+    def post(self, room):
+        args = parser.parse_args()
+        username = args['username']
+        if r.sismember("rooms", room):
+            if r.sismember("djset:%s" % room, username):
+                r.srem("djset:%s" % room, username) 
+                ret= {
+                    'room': room,
+                    'success': True,
+                    'removed': True,
+                    'user': username,
+                }
+
+            else:
+                ret= {
+                    'room': room,
+                    'success': False,
+                    'error': 'user is not in this room',
+                    'user': username,
+                }
+        else:
+            ret= {
+                'room': room,
+                'success': False,
+                'error': 'room does not exist',
+                'user': username,
+            }
+        return ret, 200
 ##
 ## Actually setup the Api resource routing here
 ##
 api.add_resource(SongList, '/songs')
 api.add_resource(Song, '/songs/<string:song_id>')
+api.add_resource(UserList, '/users')
+api.add_resource(User, '/users/<string:username>')
+api.add_resource(Queue, '/users/<string:username>/queue')
+api.add_resource(RoomList, '/rooms')
+api.add_resource(Room, '/rooms/<string:room>')
+api.add_resource(Skip, '/rooms/<string:room>/skip')
+api.add_resource(Join, '/rooms/<string:room>/join')
+api.add_resource(Leave, '/rooms/<string:room>/leave')
 
-@app.teardown_request
-def shutdown_session(exception=None):
-      db.session.remove()
 if __name__ == '__main__':
       app.run(debug=True)
---
+++ playlist.py
@@ -0,0 +1,55 @@
+import redis
+r = redis.Redis("localhost")
+room = 'interns'
+songnumber = -1
+title = ''
+song_title = ''
+artist = ''
+# Function called to initialize your python environment.
+# Should return 1 if ok, and 0 if something went wrong.
+def ices_init ():
+    print 'Executing initialize() function..'
+    return 1
+
+# Function called to shutdown your python enviroment.
+# Return 1 if ok, 0 if something went wrong.
+def ices_shutdown ():
+    print 'Executing shutdown() function...'
+    return 1
+
+# Function called to get the next filename to stream. 
+# Should return a string.
+def ices_get_next ():
+    print 'Executing get_next() function...'
+    username = r.rpop("djlist:%s" % room)
+    print username
+    song_hash = r.rpop("queue:%s" % username)
+    print song_hash
+    title = r.hget("song:%s" % song_hash, "title")
+    song_title = r.hget("song:%s" % song_hash, "song_title")
+    artist = r.hget("song:%s" % song_hash, "artist")
+    
+    r.hset("room:%s" % room, "current_song", song_hash)
+
+    
+    r.lpush("djlist:%s" % room, username)
+    r.lpush("queue:%s" % username, song_hash)
+    return "/home/mackieg/dev/turntable/music/%s.mp3" % song_hash
+# This function, if defined, returns the string you'd like used
+# as metadata (ie for title streaming) for the current song. You may
+# return null to indicate that the file comment should be used.
+def ices_get_metadata ():
+    if song_title != '':
+        metadata = song_title + " - " + artist
+    else:
+        metadata = title
+    return metadata
+
+# Function used to put the current line number of
+# the playlist in the cue file. If you don't care about this number
+# don't use it.
+def ices_get_lineno ():
+    global songnumber
+    print 'Executing get_lineno() function...'
+    songnumber = songnumber + 1
+    return songnumber
